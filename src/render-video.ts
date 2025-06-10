fba49b0ddc10e8bf7e704c295af9aedc556bede4
Felippe Chemello <felippechemello@gmail.com>
2025-06-10 15:56:28 -0300

    Update prompts and fix video speedup when the speed factor is bellow 1

--- src/render-video.ts
+++ src/render-video.ts
@@ -73,7 +73,7 @@ for (const script of scripts) {
         videos.push(videoPath);
 
         const videoDuration = await getVideoDurationInSeconds(videoPath);
-        if (composition === 'Portrait' && videoDuration <= MAX_DURATION_FOR_SHORT_CONVERSION) {
+        if (composition === 'Portrait' && videoDuration <= MAX_DURATION_FOR_SHORT_CONVERSION && videoDuration > MAX_DURATION_OF_SHORT_VIDEO) {
             const speedFactor = Math.ceil((videoDuration / MAX_DURATION_OF_SHORT_VIDEO) * 100) / 100;
 
             console.log(`Speeding up video by a factor of ${speedFactor} to convert to short format`);
